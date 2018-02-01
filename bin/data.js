262ad51ec07d1823212a2e96510853d2235f23bb
zihaowang <zihaowang@Ctrip.com>
2018-02-01 19:38:45 +0800

    save

---
+++ bin/data.js
@@ -0,0 +1,257 @@
+module.exports = {
+    style:'.pm-lang-en{font:12px/1.5 BlinkMacSystemFont,-apple-system, Helvetica, Arial, sans-serif;}\n' +
+    '.pm-lang-hk{font:12px/1.5 "Microsoft Jhenghei", "PingFang HK", "STHeitiTC-Light", tahoma, arial, sans-serif;}\n' +
+    '.pm-lang-hken{font:12px/1.5 BlinkMacSystemFont,-apple-system, Helvetica, Arial, sans-serif;}\n' +
+    '.pm-lang-sg{font:12px/1.5 BlinkMacSystemFont,-apple-system, Helvetica, Arial, sans-serif;}\n' +
+    '.pm-lang-kr{font:12px/1.5 "나눔바른고딕",Nanum Barun Gothic,"나눔 고딕","Nanum Gothic","맑은 고딕","Malgun Gothic","돋움",dotum,tahoma,arial,sans-serif;}\n' +
+    '.pm-lang-jp{font:12px/1.5 Meiryo UI,Meiryo, "MS PGothic", "Helvetica Neue", Helvetica, Osaka, Tahoma, Arial, Sans-serif;}\n' +
+    '.banner-con{background:url(./images/banner-en.jpg) no-repeat 50% 0;padding-top:400px;}\n' +
+    '.pm-lang-hk .banner-con{background-image:url(./images/banner-hk.jpg)}\n' +
+    '.pm-lang-hken .banner-con{background-image:url(./images/banner-en.jpg)}\n' +
+    '.pm-lang-jp .banner-con{background-image:url(./images/banner-jp.jpg)}\n' +
+    '.pm-lang-kr .banner-con{background-image:url(./images/banner-kr.jpg)}\n' +
+    '.pm-lang-sg .banner-con{background-image:url(./images/banner-en.jpg)}\n' +
+    '.pm-lang-de .banner-con{background-image:url(./images/banner-de.jpg)}\n' +
+    '.pm-lang-es .banner-con{background-image:url(./images/banner-es.jpg)}\n' +
+    '.pm-lang-fr .banner-con{background-image:url(./images/banner-fr.jpg)}\n' +
+    '.pm-lang-ru .banner-con{background-image:url(./images/banner-ru.jpg)}\n' +
+    '.pm-lang-th .banner-con{background-image:url(./images/banner-th.jpg)}\n' +
+    '.pm-lang-id .banner-con{background-image:url(./images/banner-id.jpg)}\n' +
+    '.pm-lang-my .banner-con{background-image:url(./images/banner-my.jpg)}\n' +
+    '\n' +
+    '\n' +
+    '@media screen and (max-width: 640px) {\n' +
+    '.banner-con{padding-top:50%;background:url(./images/banner-m-en.jpg) no-repeat 0 0;background-size:contain;}\n' +
+    '.pm-lang-en .banner-con{background-image:url(./images/banner-m-en.jpg)}\n' +
+    '.pm-lang-sg .banner-con{background-image:url(./images/banner-m-en.jpg)}\n' +
+    '.pm-lang-hken .banner-con{background-image:url(./images/banner-m-en.jpg)}\n' +
+    '.pm-lang-hk .banner-con{background-image:url(./images/banner-m-hk.jpg)}\n' +
+    '.pm-lang-jp .banner-con{background-image:url(./images/banner-m-jp.jpg)}\n' +
+    '.pm-lang-kr .banner-con{background-image:url(./images/banner-m-kr.jpg)}\n' +
+    '.pm-lang-de .banner-con{background-image:url(./images/banner-m-de.jpg)}\n' +
+    '.pm-lang-es .banner-con{background-image:url(./images/banner-m-es.jpg)}\n' +
+    '.pm-lang-fr .banner-con{background-image:url(./images/banner-m-fr.jpg)}\n' +
+    '.pm-lang-ru .banner-con{background-image:url(./images/banner-m-ru.jpg)}\n' +
+    '.pm-lang-th .banner-con{background-image:url(./images/banner-m-th.jpg)}\n' +
+    '.pm-lang-id .banner-con{background-image:url(./images/banner-m-id.jpg)}\n' +
+    '.pm-lang-my .banner-con{background-image:url(./images/banner-m-my.jpg)}\n' +
+    '}\n' +
+    '\n' +
+    '.pm-info p{line-height:1.3;margin-bottom:4px;}\n' +
+    '.pm-terms{font-size:12px;padding:25px 0;background-color:#f5f5f5;}\n' +
+    '.pm-terms .tit{font-size:14px;margin-bottom:10px;font-weight:bold;}\n' +
+    '.pm-terms .list{list-style:decimal;padding-left:30px;}\n' +
+    '.pm-terms ol li ul{padding-left:20px;}\n' +
+    '.pm-terms ol li ul li{list-style:initial;}\n' +
+    '.pm-info{background-color:#3b7ac0;color:#fff;font-size:16px;line-height:1;}\n' +
+    '\n',
+    html:'<!DOCTYPE HTML>\n' +
+    '<html lang="en-US">\n' +
+    '<head>\n' +
+    '    <meta charset="UTF-8">\n' +
+    '    <title>Lunar New Year Deals | Trip.com</title>\n' +
+    '    <meta name="viewport" content="width=device-width, initial-scale=1.0" />\n' +
+    '    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />\n' +
+    '\t<link rel="stylesheet" href="//pages.trip.com/css/v3/pm-base.css"/>\n' +
+    '    <link rel="stylesheet" href="//pages.trip.com/css/v4/global-s.css"/>\n' +
+    '    <link rel="stylesheet" href="style.css" />\n' +
+    '    <script type="text/javascript" src="//pages.trip.com/js/lib/jquery-1.11.3.min.js"></script>\n' +
+    '    <script type="text/javascript" src="//pages.trip.com/js/v4/header/header-en.js"></script>\n' +
+    '    <script type="text/javascript" src="//pages.trip.com/js/market/tab-hotel-gallery.js"></script>\n' +
+    '    <script type="text/javascript" src="//pages.trip.com/js/market/tab-flight-gallery.js"></script>\n' +
+    '</head>\n' +
+    '<body>\n' +
+    '<div class="pm-lang-hken">\n' +
+    '\n' +
+    '\t<div class="pm-banner-con banner-con"></div>\n' +
+    '\n' +
+    '\t<div class="pm-info">\n' +
+    '\t\t<div class="pm-inner">\n' +
+    '\t\t\t<p>Bring in the Lunar New Year in the best possible way - with a holiday! Trip.com has some great deals on flights and hotels but these prices can\'t last!</p>\n' +
+    '\t\t</div>\n' +
+    '\t</div>\n' +
+    '\n' +
+    '\t<!--机酒-->\n' +
+    '\t<h2 class="title flight-tit" id="flight-tit">Hot Return Flight Deals</h2>\n' +
+    '\t<div class="pm-flight-block" id="pm-flight-block-0000001">\n' +
+    '\t\t<div class="pm-center"><div class="pm-block-loading"><span class="mc-loading-36-blue"></span></div></div>\n' +
+    '\t\t<script type="text/javascript">\n' +
+    '            (new TabFlightGallery({\n' +
+    '                promoID: 130,\n' +
+    '                locale:"en_au",\n' +
+    '                rootId: \'pm-flight-block-0000001\'\n' +
+    '            })).init();\n' +
+    '\t\t</script>\n' +
+    '\t</div>\n' +
+    '\t<h2 class="title hotel-tit">Popular hotel deals</h2>\n' +
+    '\t<div class="pm-hotel-block" id="pm-hotel-block-0000001">\n' +
+    '\t\t<div class="pack-loading pm-center"><span class="mc-loading-36-blue"></span></div>\n' +
+    '\t\t<script type="text/javascript">\n' +
+    '            (new TabHotelGallery({\n' +
+    '                promoID: 130,\n' +
+    '                language: \'EN\',\n' +
+    '                market: \'AU\',\n' +
+    '                currency: \'AUD\',\n' +
+    '                checkIn: \'2018-05-01\',\n' +
+    '                checkOut: \'2018-05-31\',\n' +
+    '                rootId: \'pm-hotel-block-0000001\'\n' +
+    '            })).init();\n' +
+    '\t\t</script>\n' +
+    '\t</div>\n' +
+    '\t<!---->\n' +
+    '\t<div class="pm-terms">\n' +
+    '\t\t<div class="pm-inner">\n' +
+    '\t\t\t<h2 class="pm-terms__tit">Terms & Conditions</h2>\n' +
+    '\t\t\t<ol class="pm-terms__list">\n' +
+    '\t\t\t\t<li>Promo period: Jan 8 to Feb 28, 2018</li>\n' +
+    '\t\t\t\t<li>Prices are correct at time of publication and are subject to availability and change. Please see trip.com to confirm availability, prices and applicable terms and conditions.</li>\n' +
+    '\t\t\t\t<li>Hotel cancellation and prepayment policies may vary according to room type. Please refer to the room policies upon reservation.</li>\n' +
+    '\t\t\t\t<li>Airlines reserve the right to adjust prices and control seat availability according to sales situation. Final fare based on airline\'s actual sale price. Seat availability subject to airlines.</li>\n' +
+    '\t\t\t\t<li>Special fares may be subject to strict change, refund and endorsement conditions. Please refer to conditions of confirmed booking for details.</li>\n' +
+    '\t\t\t\t<li>Trip.com reserves all rights of final interpretation.</li>\n' +
+    '\t\t\t</ol>\n' +
+    '\t\t</div>\n' +
+    '\t</div>\n' +
+    '\t\n' +
+    '</div>\n' +
+    '\n' +
+    '<script type="text/javascript" src="//webresource.english.c-ctrip.com/resaresenglish/ibu/fe-market/js/cquery-with-pro.bc62000a.js"></script>\n' +
+    '<script type="text/javascript" src="//pages.trip.com/js/v4/footer/footer-${test}.js"></script>\n' +
+    '<input id="page_id" type="hidden" value="" />\n' +
+    '\n' +
+    '</body>\n' +
+    '</html>',
+    dicList: [
+        "design",
+        "Design",
+        "au",
+        "AU",
+        "en",
+        "En",
+        "hken",
+        "HKEN",
+        "sg",
+        "SG",
+        "de",
+        "DE",
+        "es",
+        "ES",
+        "id",
+        "ID",
+        "fr",
+        "FR",
+        "jp",
+        "JP",
+        "kr",
+        "KR",
+        "my",
+        "MY",
+        "ru",
+        "RU",
+        "th",
+        "TH",
+        "hk",
+        "HK"
+    ],
+    lanList: {
+        "en-au.html": {
+            "js":"au",
+            "locale":"en-au",
+            "language":"EN",
+            "market":"AU",
+            "currency":"AUD"
+        },
+        "en-us.html": {
+            "js":"en",
+            "locale":"en-us",
+            "language":"EN",
+            "market":"EN",
+            "currency":"USD"
+        },
+        "en-hk.html": {
+            "js":"hken",
+            "locale":"en-hk",
+            "language":"EN",
+            "market":"HK",
+            "currency":"HKD"
+        },
+        "en-sg.html": {
+            "js":"sg",
+            "locale":"en-sg",
+            "language":"EN",
+            "market":"SG",
+            "currency":"SGD"
+        },
+        "de-de.html": {
+            "js":"de",
+            "locale":"de-de",
+            "language":"DE",
+            "market":"DE",
+            "currency":"EUR"
+        },
+        "es-es.html": {
+            "js":"es",
+            "locale":"es-es",
+            "language":"ES",
+            "market":"ES",
+            "currency":"EUR"
+        },
+        "id-id.html": {
+            "js":"id",
+            "locale":"id-id",
+            "language":"ID",
+            "market":"ID",
+            "currency":"IDR"
+        },
+        "fr-fr.html": {
+            "js":"fr",
+            "locale":"fr-fr",
+            "language":"FR",
+            "market":"FR",
+            "currency":"EUR"
+        },
+        "ja-jp.html": {
+            "js":"jp",
+            "locale":"ja-jp",
+            "language":"JP",
+            "market":"JP",
+            "currency":"JPY"
+        },
+        "ko-kr.html": {
+            "js":"kr",
+            "locale":"ko-kr",
+            "language":"KR",
+            "market":"KR",
+            "currency":"KRW"
+        },
+        "ms-my.html": {
+            "js":"my",
+            "locale":"ms-my",
+            "language":"MY",
+            "market":"MY",
+            "currency":"MYR"
+        },
+        "ru-ru.html": {
+            "js":"ru",
+            "locale":"ru-ru",
+            "language":"RU",
+            "market":"RU",
+            "currency":"RUB"
+        },
+        "th-th.html": {
+            "js":"th",
+            "locale":"th-th",
+            "language":"TH",
+            "market":"TH",
+            "currency":"THD"
+        },
+        "zh-hk.html": {
+            "js":"hk",
+            "locale":"zh-hk",
+            "language":"TC",
+            "market":"HK",
+            "currency":"HKD"
+        }
+    },
+    test:"111"
+}
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
+var app = require('./bin/app.js');
+var program = require('commander');
 
-'use strict';
+function image(url) {
+    app.image(url);
+}
 
-require('./bin/app');
+program.option('-u,--url <value>','url of image dictionary',image)
+    .command('init')
+    .action(function () {
+        app.init();
+    })
+
+program.parse(process.argv);
